31bf7612e02ceeb079fb1459287b64f6f9279312
GARALAAN-VISHNUVARDHAN <vvishnuvardhan58@gmail.com>
2026-08-09 11:28:33 +0530

    Change database URL to use DATABASE_URL environment variable
    Updated database connection URL to use environment variable.

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
+
 from sqlalchemy.orm import sessionmaker
+import os
+
+db_url = os.getenv("DATABASE_URL")
 # db_url="postgresql://postgres:newpassword@localhost:5432/shop_db"
-db_url = "postgresql://postgres:newpassword@db:5432/shop_db"
+# db_url = "postgresql://postgres:newpassword@db:5432/shop_db"
 engine=create_engine(db_url)
 session=sessionmaker(autoflush=False,autocommit=False,bind=engine)
 
